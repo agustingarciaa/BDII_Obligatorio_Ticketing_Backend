f5d9a3bec4046445e9fd028793550a5c2aa44cac
Agustin Garcia <agustin.garciab@correo.ucu.edu.uy>
2026-06-16 12:50:40 -0300

    Stats (#35)
    * Add/Usuarios Module and its endpoints
    
    * Fix/funcionalidades Usuario
    
    * Add/ Estadisticas para el backend
    
    * Update de Stats

--- backend_ticketing/src/estadisticas/estadisticas.controller.ts
+++ backend_ticketing/src/estadisticas/estadisticas.controller.ts
@@ -14,8 +14,23 @@ export class EstadisticasController {
     return this.estadisticasService.masVendidos(user.role);
   }
 
+  @Get('partidos/menos-vendidos')
+  menosVendidos(@CurrentUser() user: AuthUser) {
+    return this.estadisticasService.menosVendidos(user.role);
+  }
+
+  @Get('sectores/mas-populares')
+  sectoresMasPopulares(@CurrentUser() user: AuthUser) {
+    return this.estadisticasService.sectoresMasPopulares(user.role);
+  }
+
   @Get('usuarios/mayores-compradores')
   mayoresCompradores(@CurrentUser() user: AuthUser) {
     return this.estadisticasService.mayoresCompradores(user.role);
+  }
+
+  @Get('equipos/populares')
+  equiposPopulares(@CurrentUser() user: AuthUser) {
+    return this.estadisticasService.equiposPopulares(user.role);
   }
 }
